19b68e8eadefc4a4755b99bde0079848c71d90bf
msvalina <marijan.svalina@gmail.com>
2011-09-20 20:06:50 +0200

    slug napokon radi

--- c_ipsite/blog/urls.py
+++ c_ipsite/blog/urls.py
@@ -1,8 +1,24 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('blog.views',
-    (r'^$', 'index'),
-    (r'^(?P<year>\d+)/$', 'year_view'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'year_month_view'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/$', 'year_month_day_view'),
+    url(r'^$', 
+        view='index',
+        name='blog_index'
+    ),
+    url(r'^(?P<year>\d+)/$',
+        view='year_archive',
+        name='blog_year_view'
+    ),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+        view='year_month_archive',
+        name='blog_month_archive'
+    ),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/$',
+        view='day_archive',
+        name='blog_day_archive'
+    ),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+        view='post_detail',
+        name='blog_detail'
+    ),
 )
--- c_ipsite/blog/views.py
+++ c_ipsite/blog/views.py
@@ -10,17 +10,23 @@ def index(request):
 
     return render_to_response('blog/index2.html', {'zadnji_naslov' : zadnji_naslov, 'ime_bloga' : ime_bloga, 'lista_naslova' : lista_naslova, 'autor' : autor })
 
-def year_view(request, year):
+def year_archive(request, year):
     ime_bloga = Blog.objects.get(pk=1)
     ent = Entry.objects.all().filter(datum_objave__year=year)
     return render_to_response('blog/detail.html', {'ent' : ent, 'ime_bloga': ime_bloga })
 
-def year_month_view(request, year, month):
+def year_month_archive(request, year, month):
     ime_bloga = get_object_or_404(Blog, pk=1)
     ent = Entry.objects.all().filter(datum_objave__year=year, datum_objave__month=month)
     return render_to_response('blog/year_month.html', {'ent': ent, 'ime_bloga': ime_bloga })
 
-def year_month_day_view(request, year, month, day):
+def day_archive(request, year, month, day):
     ime_bloga = get_object_or_404(Blog, pk=1)
     ent = Entry.objects.all().filter(datum_objave__year=year, datum_objave__month=month, datum_objave__day=day)
     return render_to_response('blog/day_archive.html', {'ent': ent, 'ime_bloga': ime_bloga })
+
+def post_detail(request, year, month, day, slug):
+    ime_bloga = get_object_or_404(Blog, pk=1)
+    ent = Entry.objects.all().filter(datum_objave__year=year, datum_objave__month=month, datum_objave__day=day, slug=slug)
+    return render_to_response('blog/post_detail.html', {'ent': ent, 'ime_bloga': ime_bloga })
+
